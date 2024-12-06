2e337b413707489a14306cdaaa1e4d9f4ae0841f
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2024-12-06 18:50:53 +0530

    Added promt template and custom promt with arguments sample

--- src/SemanticKernel/LearnDemo/Program.cs
+++ src/SemanticKernel/LearnDemo/Program.cs
@@ -2,15 +2,17 @@
 using Microsoft.SemanticKernel.Plugins.Core;
 
 class Program
-{ 
-static string yourDeploymentName = "gpt-4-32k";
-static string yourEndpoint = "https://pctesopenaicentral.openai.azure.com/";
-static string yourKey = "0bf5d78b38a5487a9a999c9bea8e4f72";
+{
+    static string yourDeploymentName = "gpt-4-32k";
+    static string yourEndpoint = "https://pctesopenaicentral.openai.azure.com/";
+    static string yourKey = "0bf5d78b38a5487a9a999c9bea8e4f72";
 
     public static async Task Main()
     {
         //await SimpleCall();
-        await PluginCall();        
+        //await PluginCall();
+        //await PromptTemplateCall();
+        await CustomPromptCall();
     }
 
     static async Task SimpleCall()
@@ -46,6 +48,75 @@ static string yourKey = "0bf5d78b38a5487a9a999c9bea8e4f72";
 
         var result = await kernel.InvokeAsync("ConversationSummaryPlugin", "GetConversationActionItems", new() { { "input", input } });
 
+        Console.WriteLine(result);
+    }
+
+    static async Task PromptTemplateCall()
+    {
+        var builder = Kernel.CreateBuilder();
+        builder.AddAzureOpenAIChatCompletion(
+            yourDeploymentName,
+            yourEndpoint,
+            yourKey,
+            "gpt-4-32k");
+
+#pragma warning disable SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+#pragma warning restore SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        var kernel = builder.Build();
+
+        string history = @"In the heart of my bustling kitchen, I have embraced 
+    the challenge of satisfying my family's diverse taste buds and 
+    navigating their unique tastes. With a mix of picky eaters and 
+    allergies, my culinary journey revolves around exploring a plethora 
+    of vegetarian recipes.
+
+    One of my kids is a picky eater with an aversion to anything green, 
+    while another has a peanut allergy that adds an extra layer of complexity 
+    to meal planning. Armed with creativity and a passion for wholesome 
+    cooking, I've embarked on a flavorful adventure, discovering plant-based 
+    dishes that not only please the picky palates but are also heathy and 
+    delicious.";
+
+        string prompt = @"This is some information about the user's background: 
+    {{$history}}
+
+    Given this user's background, provide a list of relevant recipes.";
+
+        var result = await kernel.InvokePromptAsync(prompt,
+            new KernelArguments() { { "history", history } });
+
+        Console.WriteLine(result);
+    }
+
+    static async Task CustomPromptCall()
+    {
+        var builder = Kernel.CreateBuilder();
+        builder.AddAzureOpenAIChatCompletion(
+            yourDeploymentName,
+            yourEndpoint,
+            yourKey,
+            "gpt-4-32k");
+
+        var kernel = builder.Build();
+
+        string language = "hindi";
+        string history = @"I'm traveling with my kids and one of them 
+    has a peanut allergy.";
+
+        string prompt = @$"Consider the traveler's background:
+    ${history}
+
+    Create a list of helpful phrases and words in 
+    ${language} a traveler would find useful.
+
+    Group phrases by category. Include common direction 
+    words. Display the phrases in the following format: 
+    Hello - Ciao [chow]";
+
+        var result = await kernel.InvokePromptAsync(prompt,
+            new KernelArguments() { { "history", history },
+                { "language", language } });
         Console.WriteLine(result);
     }
 }
