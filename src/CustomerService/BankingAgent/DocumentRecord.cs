9ee39d32e841deed90f81cc9bda8b9e6ec98bf9e
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2025-01-23 20:48:46 +0530

    added vector generation logic using InMemory store.

---
+++ src/CustomerService/BankingAgent/DocumentRecord.cs
@@ -0,0 +1,27 @@
+﻿namespace BankingAgent;
+
+using Microsoft.Extensions.VectorData;
+using System;
+
+internal class DocumentRecord
+{
+    /// <summary>A unique key for the text paragraph.</summary>
+    [VectorStoreRecordKey]
+    public required String Key { get; init; }
+
+    /// <summary>A uri that points at the original location of the document containing the text.</summary>
+    [VectorStoreRecordData]
+    public required String DocumentUri { get; init; }
+
+    /// <summary>The id of the paragraph from the document containing the text.</summary>
+    [VectorStoreRecordData]
+    public required String ParagraphId { get; init; }
+
+    /// <summary>The text of the paragraph.</summary>
+    [VectorStoreRecordData]
+    public required String Text { get; init; }
+
+    /// <summary>The embedding generated from the Text.</summary>
+    [VectorStoreRecordVector(1536)]
+    public ReadOnlyMemory<float> TextEmbedding { get; set; }
+}
--- src/CustomerService/BankingAgent/Program.cs
+++ src/CustomerService/BankingAgent/Program.cs
@@ -2,13 +2,25 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Embeddings;
+using SharedLibrary;
+using System.Runtime.CompilerServices;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+
+#pragma warning disable SKEXP0010
+#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0020
 
 internal class Program
 {
     static async Task Main()
     {
+        await EmbedToVectorStore();
+
         var builder = Kernel.CreateBuilder()
                      .AddAzureOpenAIChatCompletion(SharedLibrary.AppSetting.DeploymentName, SharedLibrary.AppSetting.Endpoint, SharedLibrary.AppSetting.Key);
 
@@ -40,8 +52,70 @@ internal class Program
             history.AddMessage(response.Role, response.InnerContent.ToString());
             Console.WriteLine(response);
 
-        } while(!String.IsNullOrEmpty(input));
+        } while (!String.IsNullOrEmpty(input));
+    }
 
-        
+    private static async Task EmbedToVectorStore()
+    {
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+        kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
+                deploymentName: AppSetting.EmbeddingModelDeploymentName!,
+                endpoint: AppSetting.Endpoint!,
+                apiKey: AppSetting.Key!)
+            .AddInMemoryVectorStore();
+
+        // Register the data uploader.
+        kernelBuilder.Services.AddSingleton<DataUploader>();
+
+        Kernel kernel = kernelBuilder.Build();
+
+        var dataUploader = kernel.Services.GetRequiredService<DataUploader>();
+        var vectorStore = kernel.Services.GetRequiredService<IVectorStore>();
+
+        var prompt = "Semantic Kernel is a lightweight, open-source development kit that lets you easily build AI agents and integrate the latest AI models into your C#, Python, or Java codebase.";
+
+        var record = dataUploader.GetDocumentRecord(prompt);
+
+        await dataUploader.GenerateEmbeddingsAndUpload("sk-documentation", [record]);        
+    }
+}
+
+internal class DataUploader(IVectorStore vectorStore, ITextEmbeddingGenerationService textEmbeddingGenerationService)
+{
+    /// <summary>
+    /// Generate an embedding for each text paragraph and upload it to the specified collection.
+    /// </summary>
+    /// <param name="collectionName">The name of the collection to upload the text paragraphs to.</param>
+    /// <param name="textParagraphs">The text paragraphs to upload.</param>
+    /// <returns>An async task.</returns>
+    public async Task GenerateEmbeddingsAndUpload(string collectionName, IEnumerable<DocumentRecord> textParagraphs)
+    {
+        var collection = vectorStore.GetCollection<string, DocumentRecord>(collectionName);
+        await collection.CreateCollectionIfNotExistsAsync();
+
+        foreach (var paragraph in textParagraphs)
+        {
+            // Generate the text embedding.
+            Console.WriteLine($"Generating embedding for paragraph: {paragraph.ParagraphId}");
+            paragraph.TextEmbedding = await textEmbeddingGenerationService.GenerateEmbeddingAsync(paragraph.Text);
+
+            // Upload the text paragraph.
+            Console.WriteLine($"Upserting paragraph: {paragraph.ParagraphId}");
+            await collection.UpsertAsync(paragraph);
+
+            Console.WriteLine();
+        }
+    }
+
+    internal DocumentRecord GetDocumentRecord(string prompt)
+    {
+        return new DocumentRecord
+        {
+            Key = Guid.NewGuid().ToString(),
+            DocumentUri = "https://www.microsoft.com",
+            ParagraphId = Guid.NewGuid().ToString(),
+            Text = prompt,
+            //TextEmbedding = readOnlyMemory
+        };
     }
 }
