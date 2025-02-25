b203042882ee936477bc288f90bfed1c8009ef4a
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2025-02-25 16:10:34 +0530

    added basic chat feature to banking mas bot

--- src/BankingMAS/BankingMAS/Program.cs
+++ src/BankingMAS/BankingMAS/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace BankingMAS
+﻿namespace BankingMAS;
+
+using BankingMAS.CommonAgent;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+internal class Program
 {
-    internal class Program
+    private static Kernel? kernel;
+
+    static async Task Main()
     {
-        static void Main(string[] args)
+        var builder = KernelFactory.GetKernelBuilder();
+        kernel = builder.Build();       
+
+        var logger = kernel.GetRequiredService<ILogger<Program>>();
+
+        var chatCompletionSevice = kernel.GetRequiredService<IChatCompletionService>();
+        var promptExecutionSettings = new PromptExecutionSettings()
         {
-            Console.WriteLine("Hello, World!");
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        };
+
+        var history = new ChatHistory();
+
+        var systemMessage = @"you are banking assistant who help answer general banking queries for a 'fast investment' banking firm customers. \
+                              You can also integrate with other systems and agents to pull out details and provide answers and take actions. You can perform various actions like accounting, policy check, loan approval, loan application, credit check, credit card check, paymentetc. ";
+
+        history.AddSystemMessage(systemMessage);
+
+        //String? input = body;
+
+        while (true)
+        {
+            Console.WriteLine("Enter your query:");
+            var query = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(query))
+            {
+                break;
+            }
+
+            history.AddUserMessage(query);
+            var response = await chatCompletionSevice.GetChatMessageContentAsync(history, promptExecutionSettings, kernel);
+            history.AddMessage(response.Role, response.InnerContent!.ToString()!);
+            Console.WriteLine(response);
+            Console.WriteLine($"result: {response.InnerContent}");
         }
     }
 }
