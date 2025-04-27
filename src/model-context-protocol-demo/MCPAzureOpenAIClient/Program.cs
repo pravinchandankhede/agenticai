3e6f4037ef3b68a6258ad38df2635bb11ef3e649
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2025-04-27 19:16:09 +0530

    added placeholder for azure openai client

---
+++ src/model-context-protocol-demo/MCPAzureOpenAIClient/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MCPAzureOpenAIClient;
+
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+using SharedLibrary;
+using System.ClientModel;
+
+internal class Program
+{
+	static void Main()
+	{
+		AzureOpenAIClient azureClient = new(
+			new Uri(AppSetting.Endpoint), 
+			new ApiKeyCredential(AppSetting.Key));
+		ChatClient chatClient = azureClient.GetChatClient(AppSetting.DeploymentName);
+				
+		Console.WriteLine("Hello, World!");
+	}
+}
