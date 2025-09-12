41309f2a0439c9d134770f5ad9953074cb156b5e
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2025-09-12 19:09:11 +0530

    added agent orchestration solution

---
+++ src/AgentOrchestration/SharedLibrary/AppSetting.cs
@@ -0,0 +1,15 @@
+﻿namespace SharedLibrary;
+
+public class AppSetting
+{
+    public static String DeploymentName = "{}";
+    public static String Endpoint = "{}";
+    public static String Key = "{}";
+    public static String EmbeddingModelDeploymentName = "{}";
+    public static String AzureSearchURL = "{}";
+    public static String AzureSearchKey = "{}";
+
+    public static String ServiceBusConnectionString = "{}";
+    public static String TopicName = "{}";
+    public static String SubscriptionName = "{}";
+}
