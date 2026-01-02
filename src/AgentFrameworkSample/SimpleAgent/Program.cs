32257056c439e784f11716b880887a232bc20808
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2026-01-02 10:11:00 +0000

    added simple agent

---
+++ src/AgentFrameworkSample/SimpleAgent/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+AIAgent agent = new AzureOpenAIClient(
+    new Uri("https://<myresource>.openai.azure.com"),
+    new AzureCliCredential())
+        .GetChatClient("gpt-4o-mini")
+        .CreateAIAgent(instructions: "You are good at telling jokes.", name: "Joker", description: "An agent that tells jokes.")
+        ;
+
+Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
+
+ChatMessage message = new(ChatRole.User, [
+    new TextContent("Tell me a joke about this image?"),
+    new UriContent("https://upload.wikimedia.org/wikipedia/commons/1/11/Joseph_Grimaldi.jpg", "image/jpeg")
+]);
+
+Console.WriteLine(await agent.RunAsync(message));
+
