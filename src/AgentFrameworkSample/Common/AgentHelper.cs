d74b05832a25fd2abeaa0655c77a6b92ef04354a
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2026-01-05 12:58:55 +0000

    added a common project to create agent. Also added an Image Agent that analyze given images.

---
+++ src/AgentFrameworkSample/Common/AgentHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+namespace Common;
+
+public static class AgentHelper
+{
+    public static AIAgent GetAgent(String name = "Joker"
+            , string description = "An agent that tells jokes."
+            , string instructions = "You are good at telling jokes.")
+    {
+        AIAgent agent = new AzureOpenAIClient(
+        new Uri(SharedLibrary.AppSetting.Endpoint),
+        new AzureKeyCredential(SharedLibrary.AppSetting.Key))
+            .GetChatClient(SharedLibrary.AppSetting.DeploymentName)
+            .AsIChatClient()
+            .CreateAIAgent(instructions: instructions, name: name, description: description)
+            ;
+
+        return agent;
+    }
+}
---
+++ src/AgentFrameworkSample/ImageAgent/Program.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using Microsoft.Extensions.AI;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var agent = AgentHelper.GetAgent(name: "ImageAgent"
+        , description: "An agent that help understand images."
+        , instructions: "You are good at describing & analyzing images");
+
+        ChatMessage message = new(ChatRole.User, [
+            new TextContent("What do you see in this image?"),
+    new UriContent("https://upload.wikimedia.org/wikipedia/commons/1/11/Joseph_Grimaldi.jpg", "image/jpeg")
+        ]);
+
+        Console.WriteLine(await agent.RunAsync(message));
+    }
+}
