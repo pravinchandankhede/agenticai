28e149343ec0944674873297ae9a7b14d6caea5b
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2026-01-09 10:10:14 +0000

    added a tool calling sample with agent framework

--- src/AgentFrameworkSample/Common/AgentHelper.cs
+++ src/AgentFrameworkSample/Common/AgentHelper.cs
@@ -1,9 +1,9 @@
-﻿using Azure;
+﻿namespace Common;
+
+using Azure;
 using Azure.AI.OpenAI;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
-
-namespace Common;
 
 public static class AgentHelper
 {
@@ -17,6 +17,22 @@ public static class AgentHelper
             .GetChatClient(SharedLibrary.AppSetting.DeploymentName)
             .AsIChatClient()
             .CreateAIAgent(instructions: instructions, name: name, description: description)
+            ;
+
+        return agent;
+    }
+
+    public static AIAgent GetAgent(String name = "Joker"
+            , string description = "An agent that tells jokes."
+            , string instructions = "You are good at telling jokes."
+            , IList<AITool>? tools = null)
+    {
+        AIAgent agent = new AzureOpenAIClient(
+        new Uri(SharedLibrary.AppSetting.Endpoint),
+        new AzureKeyCredential(SharedLibrary.AppSetting.Key))
+            .GetChatClient(SharedLibrary.AppSetting.DeploymentName)
+            .AsIChatClient()
+            .CreateAIAgent(instructions: instructions, name: name, description: description, tools)
             ;
 
         return agent;
---
+++ src/AgentFrameworkSample/ToolingAgent/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+using Common;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using ToolingAgent;
+
+internal class Program
+{
+    private static async Task Main()
+    {
+        AIAgent agent = AgentHelper.GetAgent(
+            "Tooling Agent",
+            "An agent that demonstrates tooling capabilities.",
+            "tooling-agent",
+            [AIFunctionFactory.Create(WeatherTool.GetWeather)]
+        );
+
+        Console.WriteLine(await agent.RunAsync("What is the weather like in Mumbai?"));
+    }
+}
