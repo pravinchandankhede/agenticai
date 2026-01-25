bd5b7b1632c597ea447940ecda844af20fd7ae2f
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2026-01-25 08:15:11 +0000

    added a simple copilot sample

---
+++ src/GitHubCopilot/SimpleAgent/Program.cs
@@ -0,0 +1,31 @@
+﻿using GitHub.Copilot.SDK;
+
+namespace GitHubCopilot.SimpleAgent;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await using var client = new CopilotClient();
+        await client.StartAsync();
+        await using var session = await client.CreateSessionAsync(new SessionConfig { Model = "gpt-4.1" });
+
+        var done = new TaskCompletionSource();
+
+        session.On(evt =>
+        {
+            if (evt is AssistantMessageEvent msg)
+            {
+                Console.WriteLine(msg.Data.Content);
+            }
+            else if (evt is SessionIdleEvent)
+            {
+                done.SetResult();
+            }
+        });
+        // Send a message and wait for completion
+        await session.SendAsync(new MessageOptions { Prompt = "What is 2+2?" });
+        await done.Task;
+        
+    }
+}
