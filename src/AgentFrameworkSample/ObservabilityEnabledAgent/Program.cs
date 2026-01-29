b541002cc520d3d87ad3433d550b31ce01ef2be1
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2026-01-29 09:03:22 +0000

    Added an agent that demonstrate observability hooks

---
+++ src/AgentFrameworkSample/ObservabilityEnabledAgent/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AgentFrameworkSample.ObservabilityEnabledAgent;
+
+using Common;
+using Microsoft.Agents.AI;
+using System;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+internal class Program
+{
+    private static async Task Main()
+    {     
+
+        // Create a TracerProvider that exports to the console
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("opentelemetrysource")
+            .AddConsoleExporter()
+            .Build();
+    
+        var agent = AgentHelper.GetAgent(
+            name: "ObservabilityEnabledAgent",
+            description: "An agent that demonstrates observability features.",
+            instructions: "You are an observability-enabled agent that provides detailed logs and metrics for each action you take."
+            );
+        agent.AsBuilder()
+        .UseOpenTelemetry("opentelemetrysource", (options) => {             
+            // Configure OpenTelemetry options here if needed
+        })
+        .Build();
+
+        Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
+    }
+}
